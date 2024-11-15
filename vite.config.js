d2c2bf2e1ebd6cc5b2bddff504d6a47b99336ca6
Sivabalan M <122948467+Sivabalan3@users.noreply.github.com>
2024-11-15 15:37:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src', // This sets up an alias for the "src" directory
+    },
+  },
   server: {
-    port: 3002,
+    port: 3002, // Set the development server port
   },
 });
+
 
