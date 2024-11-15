6e5040b233a9fb8a3068462123c49a36a2abda9a
Sivabalan M <122948467+Sivabalan3@users.noreply.github.com>
2024-11-15 15:24:05 +0530

    Update MasterTable.jsx

--- src/MasterExcel/MasterTable.jsx
+++ src/MasterExcel/MasterTable.jsx
@@ -10,7 +10,6 @@ import {
   fetchExcelData,
 } from "../store/masterexcelfile/masterExcelFileSlice";
 import { useDispatch } from "react-redux";
-// import MasterUploadFile from "./Masteruploadfile";
 import MasterUploadFile from "./Masteruploadfile";
 const DataTable = ({ data }) => {
   const [isOpen, setIsOpen] = useState(false);
